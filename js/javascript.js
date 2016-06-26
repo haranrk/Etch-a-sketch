13bc7299589aaf8964d1614a8e7ea8a1788a9fb2
haranrk <haranbolt@gmail.com>
2016-06-26 18:56:28 +0530

    Basic requirement completed

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,36 @@
+console.log("JS start loaded");
+var m=16, n=16;
+
 $(document).ready(function(){
-	$('h1').text("hello");
+	play();		
 }); 
 
-console.log("hello");
+function play(){
+for (var i = 0; i < n; i++) {
+		$("#grid").append('<div class="row"></div>');
+	}
+	for (var i = 0; i < m; i++) {
+		$(".row").append('<div class="sq"></div>')
+	}
+	$(".sq").mouseover(function() {
+		$(this).css("background-color","#ECF0F1")
+	});
+
+	$(".sq").click(function(){
+		$(this).css("background-color","#2980B9")
+	})
+}
+
+function replay(){
+	$("#grid").empty();
+	m = prompt("Width?");
+	n = prompt("Height?");
+	play();
+}
+
+console.log("JS end loaded");
+/*.mouseenter(
+		$(".sq").hover(,)
+	);
+
+	*/
